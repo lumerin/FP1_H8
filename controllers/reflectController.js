ea208d0a9adfc2808d6d844152e52e24a9e9295a
Zhafira Hajar <zfrhjr@gmail.com>
2021-11-17 21:42:47 +0700

    fix: add validation on update dat

--- controllers/reflectController.js
+++ controllers/reflectController.js
@@ -1,39 +1,36 @@
-const Reflection= require("../models/reflectionModel");
+const Reflection = require('../models/reflectionModel');
 
 class Reflect {
-    static create (req, res) {
-        const data = { 
-            success: req.body.success,
-            low_point: req.body.low_point,
-            take_away: req.body.take_away,
-            owner_id: req.auth.id
-         };
-        Reflection.insert(res, data);
-    }
-    
-    static take (req, res) {
-        const owner_id = req.auth.id;
-        Reflection.read(res, owner_id);
-    }
+	static create(req, res) {
+		let data = {
+			success: req.body.success,
+			low_point: req.body.low_point,
+			take_away: req.body.take_away,
+			owner_id: req.auth.id,
+		};
+		Reflection.insert(res, data);
+	}
 
-    static change (req, res) {
-        const data = {
-            id: req.params.id,
-            success: req.body.success,
-            low_point: req.body.low_point,
-            take_away: req.body.take_away,
-            owner_id: req.auth.id
-         };
-         Reflection.update(res, data);
-    }
+	static take(req, res) {
+		const owner_id = req.auth.id;
+		Reflection.read(res, owner_id);
+	}
 
-    static remove (req, res) {
-        const data = {
-            id: req.params.id,
-            owner_id: req.auth.id
-        };
-        Reflection.delete(res, data);
-    }
+	static change(req, res) {
+		const data = {
+			id: req.params.id,
+			owner_id: req.auth.id,
+		};
+		Reflection.update(req, res, data);
+	}
+
+	static remove(req, res) {
+		const data = {
+			id: req.params.id,
+			owner_id: req.auth.id,
+		};
+		Reflection.delete(res, data);
+	}
 }
 
 module.exports = Reflect;
